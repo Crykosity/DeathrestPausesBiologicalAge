1b90321528fda5f85059ecc82f0981b404493e92
Oscar.Carman <oscarcarman2@gmail.com>
2023-03-01 13:03:45 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Deathrest Pauses Biological Age
+
+A mod for [Rimworld](https://store.steampowered.com/app/294100/RimWorld/). You can download it on the [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2940569974). 
+
+In base Rimworld, children do not gain growth score whilst in deathrest; this avoids an exploit in which a player could bring a child up to a maximum learning need and then suspend it whilst they deathrest, gaining high growth for no effort. However, this fix has the side-effect of disadvantaging deathresting children, as there are many days where they could be learning, but are instead forced to undertake deathrest. 
+
+This mod avoids both problems by pausing biological age progression whilst a pawn deathrests. This change allows children to have the same total time to increase their growth score, while remaining thematically consistent.
+
---
+++ Source/Main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using Verse.Sound;
+using Verse.Noise;
+using Verse.Grammar;
+using RimWorld;
+using RimWorld.Planet;
+
+using System.Reflection;
+using HarmonyLib;
+
+namespace Template
+{
+
+    [StaticConstructorOnStartup]
+    public static class Start
+    {
+        static Start()
+        {
+            var harmony = new Harmony("com.username.modname");
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(Pawn_AgeTracker))]
+        [HarmonyPatch("BiologicalTicksPerTick", MethodType.Getter)]
+        class Patch {
+            static void Postfix(ref Pawn ___pawn, ref float __result)
+            {
+                if (___pawn.Deathresting) {
+                    __result = 0f;
+                }
+            }
+        }
+    }
+
+}
